dede95aba1aff529855cd3bfd67df918b47bf2a8
CyrilKym <cyril.kym@students.fhnw.ch>
2020-02-19 13:41:47 +0100

    erster prototyp mit results

--- vue_checker.js
+++ vue_checker.js
@@ -8,10 +8,12 @@ var app = new Vue({
         house_nr: '',
         plz_valid: false,
         strasse_valid: false,
-        info: '',
+        house_nr_valid: false,
         plz_orte: [],
         strassen: [],
-        ort: ''
+        house_nrs: [],
+        ort: '',
+        result: ''
     },
 
     watch: {
@@ -32,7 +34,6 @@ var app = new Vue({
 
                 //prüft ob es resultate zum anzeigen gibt
                 if(this.strassen.length>0){
-                    console.log
                     //prüft ob die eingegebene Strasse im Resultarray vorkommt
                     var found = false
                     for(var i = 0; i < this.strassen.length; i++) {
@@ -47,6 +48,25 @@ var app = new Vue({
                 }
                 this.getStrasse()
             }
+        },
+        house_nr: function(){
+            console.log("hello")
+            if(this.strassen.length == 1 && this.plz_orte.length == 1 && this.house_nr.length > 0){
+                if(this.house_nrs.length > 0){
+                    for(var i = 0; i < this.house_nrs.length; i++) {
+                        if (this.house_nrs[i].house == this.house_nr) {
+                            var found = true
+                            break;
+                        }
+                    }
+                    if(!found){
+                        this.house_nr_valid = false;
+                    }
+                }
+                
+                this.getHouseNr()
+            }
+            
         }
     },
 
@@ -72,9 +92,20 @@ var app = new Vue({
                     console.log(error)
                 })
         },
+        getHouseNr: function(){
+            var vm = this
+            axios.get(api + 'post/houses/' + this.plz + '/' + this.strasse + '/' + this.house_nr)
+                .then(function (response) {
+                    vm.house_nrs = response.data
+                    console.log(vm.house_nrs)
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        },
         select_plz: function (event) {
             this.plz_valid = true
-            splitArray = event.target.innerText.split(" ")
+            var splitArray = event.target.innerText.split(" ")
             this.plz = splitArray[0]
             splitArray.shift()
             this.ort = splitArray.join()
@@ -82,6 +113,42 @@ var app = new Vue({
         select_street: function(event){
             this.strasse_valid = true
             this.strasse = event.target.innerText
+        },
+        select_house_nr: function(event){
+            this.house_nr_valid = true
+            this.house_nr = event.target.innerText
+        },
+        check: function(){
+            console.log("clicked")
+            var vm = this
+            axios.get(api + 'check/address/' + this.plz + '/' + this.strasse + '/' + this.house_nr + '/')
+                .then(function (response) {
+                    console.log(response.data)
+                    vm.result = response.data
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
         }
-}
+    },
+    computed: {
+        info: function(){
+            return("plz_valid: " + this.plz_valid + "<br>" +
+                  "strasse_valid: " + this.strasse_valid + "<br>" +
+                  "house_nr_valid: " + this.house_nr_valid + "<br>" +
+                  "plz: " + this.plz + "<br>" +
+                  "ort: " + this.ort + "<br>" +
+                  "strasse: " + this.strasse + "<br>" +
+                  "house_nr: " + this.house_nr + "<br>" +
+                  "plz_orte: " + this.plz_orte + "<br>" +
+                  "strassen: " + this.strassen + "<br>" +
+                  "house_nrs: " + this.house_nrs + "<br>"+
+                  "result_is_fiber: " + this.result.fiber + "<br>" + 
+                  "result_is_vdsl: " + this.result.vdsl + "<br>" + 
+                  "result_is_litexchange: " + this.result.litex + "<br>" + 
+                  "result_is_vdsl_speed_up: " + this.result.vdsl_up + "<br>" + 
+                  "result_is_vdsl_speed_down: " + this.result.vdsl_down + "<br>"
+                  )
+        }
+    }
 })
